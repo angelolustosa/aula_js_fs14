1b4e7eae13d37d6596d27dc01a494c29719889d8
gleidsonteixeira <alessandrofeitoza01@gmail.com>
2023-05-04 20:25:34 -0300

    variáveis e tipos de dados

---
+++ aula_01/variaveis.js
@@ -0,0 +1,22 @@
+let teste = 'Banana'
+
+teste = 'Abacaxi'
+//console.log(teste)
+
+const teste2 = 78
+
+let estrelas = ['Anilam','Betegeulse', 'Mintaka']
+
+let usuario = {
+    nome: 'Juliano',
+    idade: '102',
+    telefone: 85932345678,
+    hobbies: ['Astronomia', 'Dev', 'Filmes'],
+    endereco: {
+        rua: 'Rua 01',
+        numero: 12,
+        bairro: 'Messejana'
+    }
+
+}
+console.log(usuario.telefone, usuario.nome, usuario.endereco.numero)
