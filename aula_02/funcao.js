6ca288baefb52c0cd2fde0de15098c081f562688
angelolustosa <angelolustosa@gmail.com>
2023-05-11 19:57:01 -0300

    funcoes, chamadas de funcao dentro de funcao

---
+++ aula_02/funcao.js
@@ -0,0 +1,22 @@
+function banana(n1, n2, n3) {
+    let resultado = (n1 + n2) - n3
+    console.log(resultado)
+}
+//banana(10, 20, 5)
+
+function soma(n1, n2) {
+    let resultado = n1 + n2
+    console.log('Soma:', resultado)
+
+    return resultado
+}
+//soma(123, 678)
+
+function media(n1, n2) {
+    //let resultado = (n1+n2)/2
+    let resultado = soma(n1, n2)/2
+    console.log('Média:', resultado)
+}
+media(123, 678)
+
+
